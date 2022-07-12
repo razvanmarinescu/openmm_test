b3b8f0ed8f04bd64cd6dbaf48c9145afb541db16
Razvan Marinescu <mrazvan22@gmail.com>
2022-07-12 10:35:54 -0700

    added files

---
+++ run_md.py
@@ -0,0 +1,32 @@
+from openmm.app import *
+from openmm import *
+from openmm.unit import *
+from sys import stdout
+import time
+start = time.time()
+
+GPU_DEVICE=1
+
+pdb = PDBFile('bstate.pdb')
+psf = CharmmPsfFile('bstate.psf')
+
+params = CharmmParameterSet('toppar_c36_jul21/top_all36_prot.rtf', 'toppar_c36_jul21/par_all36m_prot.prm')
+
+system = psf.createSystem(params, nonbondedMethod=NoCutoff, nonbondedCutoff=1*nanometer, constraints=HBonds, implicitSolvent=GBn2)
+
+integrator = LangevinIntegrator(300*kelvin, 1/picosecond, 0.002*picoseconds)
+
+platform = Platform.getPlatformByName('CUDA')
+properties = {'DeviceIndex': '%s' % GPU_DEVICE} # implicit solvent doesn't support more than 1 GPU
+simulation = Simulation(psf.topology, system, integrator, platform, properties)
+simulation.context.setPositions(pdb.positions)
+print('psb.positons', len(pdb.positions))
+
+simulation.loadState('parent.xml')
+simulation.reporters.append(DCDReporter('seg.dcd', 1000)) 
+simulation.reporters.append(StateDataReporter(stdout, 1000, step=True,
+        potentialEnergy=True, temperature=True, speed=True))
+simulation.step(25000)
+
+end = time.time()
+print('elapsed seconds:', end - start)
