bfe94fbae2dada4a475726483bb59147590f3be1
Razvan Marinescu <mrazvan22@gmail.com>
2022-07-12 14:50:13 -0700

    switched to 2nm cutoff, 470ns/day performance

--- run_md.py
+++ run_md.py
@@ -13,12 +13,31 @@ psf = CharmmPsfFile('bstate.psf')
 
 params = CharmmParameterSet('toppar_c36_jul21/top_all36_prot.rtf', 'toppar_c36_jul21/par_all36m_prot.prm')
 
-system = psf.createSystem(params, nonbondedMethod=NoCutoff, nonbondedCutoff=1*nanometer, constraints=HBonds, implicitSolvent=GBn2)
+# making heavy hydrogens is actually 30% slower, prob due to the extra constraints
+#mode = 'heavy' 
+#mode = 'noconstraints' 
+mode = 'normal'
+if mode == 'heavy': # 670ns/day
+  dt = 0.005*picoseconds
+  constraints = AllBonds
+  hydrogenMass = 4*amu
+elif mode == 'noconstraints': #230ns/day
+  dt = 0.001*picoseconds
+  constraints = None
+  hydrogenMass = 1.5*amu
+else: # 900ns/day
+  dt = 0.004*picoseconds
+  constraints = HBonds
+  hydrogenMass = 1.5*amu
+
+# for speed, add CutoffNonPeriodic, to cutoff long-range electrostatics
+# Peter Eastman: for GB, use 2nm cutoff
+system = psf.createSystem(params, nonbondedMethod=CutoffNonPeriodic, nonbondedCutoff=2*nanometer, constraints=constraints, implicitSolvent=GBn2, hydrogenMass=hydrogenMass)
 
 # Langevin Middle Integrator is more accurate for larger timesteps
 # 4fs ok, but need to keep constraints on HBonds (I think)
 # 5fs works, but need heavy hydrogens (mass redistribution from heavy atoms)
-integrator = LangevinMiddleIntegrator(300*kelvin, 1/picosecond, 0.004*picoseconds)
+integrator = LangevinMiddleIntegrator(300*kelvin, 1/picosecond, dt)
 
 platform = Platform.getPlatformByName('CUDA')
 properties = {'DeviceIndex': '%s' % GPU_DEVICE} # implicit solvent doesn't support more than 1 GPU
