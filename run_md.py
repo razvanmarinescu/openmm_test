4240c816d3ef8f421864c0cad603893621cef513
Razvan Marinescu <mrazvan22@gmail.com>
2022-07-12 13:22:00 -0700

    increased to 4fs timestep

--- run_md.py
+++ run_md.py
@@ -15,7 +15,9 @@ params = CharmmParameterSet('toppar_c36_jul21/top_all36_prot.rtf', 'toppar_c36_j
 
 system = psf.createSystem(params, nonbondedMethod=NoCutoff, nonbondedCutoff=1*nanometer, constraints=HBonds, implicitSolvent=GBn2)
 
-integrator = LangevinIntegrator(300*kelvin, 1/picosecond, 0.002*picoseconds)
+# 4fs ok, but need to keep constraints on HBonds (I think)
+# 5 fs works, but need heavy hydrogens (mass redistribution from heavy atoms)
+integrator = LangevinIntegrator(300*kelvin, 1/picosecond, 0.004*picoseconds)
 
 platform = Platform.getPlatformByName('CUDA')
 properties = {'DeviceIndex': '%s' % GPU_DEVICE} # implicit solvent doesn't support more than 1 GPU
